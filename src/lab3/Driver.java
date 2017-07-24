a5784fb04003b553468beb09d90f1ef626cfc2d2
xxuil <liuxxggwp@utexas.edu>
2017-07-24 00:12:52 -0500

    initial

---
+++ src/lab3/Driver.java
@@ -0,0 +1,28 @@
+public class Driver {
+
+    public static void main(String[] args) {
+
+	if (args.length == 0) {
+            System.err.println("usage: java Driver -n <num_courses> -h <total_num_hours>");
+            System.exit(1);
+        }
+
+        Program3 program = new Program3();
+	int n = Integer.parseInt(args[1]);
+	int h = Integer.parseInt(args[3]);
+        int sum_grade;
+        int maxgrade = 100;
+        SquareRootGradeFunction gf = new SquareRootGradeFunction(n, maxgrade);
+//      SquareGradeFunction gf = new SquareGradeFunction(n, maxgrade);
+
+        program.initialize(n, maxgrade, gf);
+        int[] hours_array = program.computeHours(h);
+        int[] grades_array = program.computeGrades(h);
+        sum_grade = 0;
+        for (int i=0; i<hours_array.length; i++) {
+            System.out.println("Class " + i + " Hours " + hours_array[i] + " Grade " + grades_array[i]);
+            sum_grade += grades_array[i];
+        }
+        System.out.println("Total Grade " + sum_grade);
+    }
+}
---
+++ src/lab3/GradeFunction.java
@@ -0,0 +1,16 @@
+public interface GradeFunction{
+
+
+/* Need to have a Constructor that initializes the number of classes
+and the max grade that can be achieved in any class
+*/
+
+    /*
+     * Returns the grade achieved for the specified class when the specified
+     * number of hours are spent on it.  For a specific run, this function
+     * should be guaranteed to give the SAME return value for any given 
+     * combination of class, hours.
+     */
+    public int grade(int classID, int hours);
+
+}
